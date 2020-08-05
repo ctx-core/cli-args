616c33189ae106caf987d73de318c5322a1ec4ae
Brian Takita <brian.takita@gmail.com>
2020-08-05 12:16:12 -0700

    fix: _param_h: return type T=any

--- _param_h.ts
+++ _param_h.ts
@@ -1,7 +1,7 @@
 import { assign, keys, pick } from '@ctx-core/object'
 import { param_dfn_h_param_name_h_type } from './param_dfn_h_param_name_h_type'
 import { _param_value_h_param_name_h } from './_param_value_h_param_name_h'
-export function _param_h(
+export function _param_h<T = any>(
 	arg_a1:string[],
 	param_dfn_h_param_name_h?:param_dfn_h_param_name_h_type,
 	default_value_h_param_name_h = {}
@@ -22,9 +22,9 @@ export function _param_h(
 	assign(param_h, default_param_h)
 	if (param_dfn_h_param_name_h) {
 		const param_name_a1 = keys(param_dfn_h_param_name_h)
-		return pick(param_h, ...param_name_a1)
+		return pick(param_h, ...param_name_a1) as T
 	}
-	return param_h
+	return param_h as T
 }
 export const _h__param = _param_h
 export const _param_h__pick__default = _param_h
